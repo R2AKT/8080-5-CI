a6752b7bde7ce5c9ac450137486580a3f2baa387
Sergey Dorozhkin (R2AKT) <r2akt@yandex.ru>
2026-08-20 23:06:41 +0300

    Update

--- modules/io/__init__.py
+++ modules/io/__init__.py
@@ -1,6 +1,9 @@
 """IO-устройства для i8080-5 CI."""
 from .iodevice import IODevice
-from .i8255 import I8255
+from .i8251 import I8251
 from .i8253 import I8253
-from .i8259 import I8259, I8259A
+from .i8255 import I8255
 from .i8257 import I8257, I8237
+from .i8259 import I8259, I8259A
+
+from .i16550 import I16550
