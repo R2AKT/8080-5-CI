d10dba53d0f4bab383293274ee202d29fd27e439
Sergey Dorozhkin (R2AKT) <r2akt@yandex.ru>
2026-08-21 15:12:49 +0300

    Update

--- modules/io/__init__.py
+++ modules/io/__init__.py
@@ -5,6 +5,9 @@ from .i8253 import I8253
 from .i8255 import I8255
 from .i8257 import I8257, I8237
 from .i8259 import I8259, I8259A
+from .i8272 import I8272
+from .i8275 import I8275
+from .i8276 import I8276
 from .i8279 import I8279
 
 from .i16550 import I16550
